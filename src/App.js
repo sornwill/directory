e68d827b0eb3c24390bff2971827819ee980c636
Soul <sornwilliam@gmail.com>
2020-08-13 16:37:54 -0500

    added react and some components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './components/Header/index';
+
+
+function App() {
+  return (
+    <div>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
